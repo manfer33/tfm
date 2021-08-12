1042c51bec369b5cb54664c9d36560cf309be4d4
Manuel J. Fdez <manfergonz@gmail.com>
2021-08-12 11:50:44 +0200

    Modify script to be used as service to manage move_arms task

--- gazebo_simulation/scripts/move_arm_nonstop.py
+++ gazebo_simulation/scripts/move_arm_nonstop.py
@@ -2,19 +2,35 @@
 
 import rospy
 from std_msgs.msg import Float64
+from std_srvs.srv import SetBool, SetBoolResponse
 
 shoulder_topic = '/aeroarms/q1_2_position_controller/command'
 
+move_arm = False
+
+def handle_arm_trigger(req):
+    global move_arm
+    move_arm = req.data
+
+    result = SetBoolResponse()
+    result.success = True
+    result.message="Arms command received: " + str(move_arm)
+
+    return result
+
 def main():
+    rospy.Service('/aeroarms/move_arm', SetBool, handle_arm_trigger)
+
     pub = rospy.Publisher(shoulder_topic, Float64, queue_size=1)
     rospy.init_node('shoulder_move', anonymous=True)
     rate = rospy.Rate(1) # 1hz
     flag = False
 
     while not rospy.is_shutdown():
-        pub.publish(0.0) if flag else pub.publish(-1.5)
-        rate.sleep()
-        flag = not flag
+        if move_arm:
+            pub.publish(0.0) if flag else pub.publish(-1.5)
+            rate.sleep()
+            flag = not flag
 
 if __name__ == "__main__":
     try:
