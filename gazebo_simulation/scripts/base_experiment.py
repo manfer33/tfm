231c4cf54a8e2790f1cdb13a161e8f4c91c838cd
Manuel J. Fdez <manfergonz@gmail.com>
2021-08-12 11:48:04 +0200

    Modify to test task using threads

--- gazebo_simulation/scripts/base_experiment.py
+++ gazebo_simulation/scripts/base_experiment.py
@@ -1,34 +1,57 @@
 #! /usr/bin/env python
 
-import rospy
+import rospy, threading
 from std_msgs.msg import Float64
-from uav_abstraction_layer.srv import TakeOff
+from geometry_msgs.msg import PoseStamped
+from uav_abstraction_layer.srv import TakeOff, GoToWaypoint
 
 shoulder_topic = '/aeroarms/q1_2_position_controller/command'
 TakeOff_ALT = 2 # [m]
 ARM_RATE = 1 # [Hz]
 
+pose = PoseStamped()
 
-def main():
-    pub = rospy.Publisher(shoulder_topic, Float64, queue_size=1)
-    takeoff = rospy.ServiceProxy('/ual/take_off', TakeOff)
+def callback_pose(data):
+    global pose
+    pose = data
 
-    rospy.init_node('shoulder_move', anonymous=True)
-    rate = rospy.Rate(ARM_RATE)
-
-    # TakeOff!
-    rospy.wait_for_service('/ual/take_off')
-    try:
-        takeoff(TakeOff_ALT, True)
-    except rospy.ServiceException as e:
-        print("Service call failed: %s"%e)
-
+def move_arm_nonstop():
     # Move arms
+    pub = rospy.Publisher(shoulder_topic, Float64, queue_size=1)
+    rate = rospy.Rate(ARM_RATE)
     flag = False
+
     while not rospy.is_shutdown():
         pub.publish(0.0) if flag else pub.publish(-1.5)
         rate.sleep()
         flag = not flag
+
+def main():
+    rospy.Subscriber("/ual/pose", PoseStamped, callback_pose)
+    
+    takeoff = rospy.ServiceProxy('/ual/take_off', TakeOff)
+    goToWp = rospy.ServiceProxy('/ual/go_to_waypoint', GoToWaypoint)
+
+    rospy.init_node('shoulder_move', anonymous=True)
+
+    # Go WP!
+    init_wp = PoseStamped()
+    init_wp.pose.position.z = 2
+    init_wp.pose.orientation = pose.pose.orientation
+
+    #TakeOff!
+    if pose.pose.position.z < 0.5:
+        rospy.wait_for_service('/ual/take_off')
+        try:
+            takeoff(TakeOff_ALT, True)
+        except rospy.ServiceException as e:
+            print("Service call failed: %s"%e)
+
+    d = threading.Thread(target=move_arm_nonstop)
+    d.setDaemon(True)
+    d.start()
+
+    rospy.spin()
 
 if __name__ == "__main__":
     try:
